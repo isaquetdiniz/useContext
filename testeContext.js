57c4403af71ae8f9ae7c70cbaf13e2e76067b6d9
Isaque Diniz <67443852+isaquetdiniz@users.noreply.github.com>
2020-09-29 15:34:02 -0300

    Update testeContext.js

--- testeContext.js
+++ testeContext.js
@@ -1,25 +1,28 @@
+//Primeiro vamos o React (para utilizar as funcionalidades dos componentes)
+//o createContext para criar um contexto
+//o useContext(para criar um hook que retornar o valor do contexto, evitando que precisemos importar o useContext nos componentes que fossemos utilizá-lo)
+//o useState (para simular que estamos guardando um contador global)
 import React, { createContext, useContext, useState } from 'react';
 
-const testeContext = createContext(null);
+//Aqui, criamos um contexto que começa null
+const TesteContext = createContext(null);
 
-export function ({ children }) {
-  const [alerts, setAlerts] = useState([]);
-  const addAlert = (alert) => {
-    setAlerts((prevAlerts) => {
-      return [...prevAlerts, alert];
-    });
-  };
+//Agora criamos o componente testeContextProvider, que recebe por props todos os seus filhos e passa como valor para os filhos o valor do contador
+export function testeContextProvider({ children }) {
+  const [count, setCount] = useState(0);
+  const addCount = () => {
+    setCount(count+=1);
+  }
   return (
-    <SnackbarContext.Provider value={addAlert}>
+    <TesteContext.Provider value={[addCount, count]}>
       {children}
-      <SnackBarContainer listAlerts={alerts} />
-    </SnackbarContext.Provider>
+    </TesteContext.Provider>
   );
 }
 
-SnackBarProvider.propTypes = {
-  children: PropTypes.element,
-};
+export const TesteContextConsumer = TesteContext.Consumer;
 
-export const SnackBarConsumer = SnackbarContext.Consumer;
-export const useSnackBar = () => useContext(SnackbarContext);
+//Aqui criamos um hook personalizado que quando importado em qualquer componente filho irá retornar os valores do nosso contexto, de modo que:
+//useContext()[0] - está guardando uma função que atualiza o estado
+//useContext()[1] - está guardando o valor do count; 
+export const useTesteContext = () => useContext(TesteContext);
